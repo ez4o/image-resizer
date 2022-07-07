c731b6355cd473115c6a2927bb2bb50d43a4a047
Xyphuz <hansen0715.2@gmail.com>
2022-07-07 19:25:07 +0800

    feat: integrate with all image type

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
-    let file: FileList | null = null;
+    let files: FileList | null = null;
 
     let width = -1;
     let height = -1;
@@ -9,16 +9,23 @@
     async function convert() {
         console.log(crossOriginIsolated);
 
-        if (file?.item(0) == null) {
+        if (files?.item(0) == null) {
             return;
         }
-        const input_file_name = file!.item(0)!.name;
+
+        const file: File = files!.item(0)!;
+
+        if (!file.type.startsWith('image/')) {
+            return;
+        }
+
+        const input_file_name = file.name;
         const output_file_name: string = "resized-" + input_file_name
 
         const ffmpeg = createFFmpeg();
         await ffmpeg.load();
 
-        ffmpeg.FS("writeFile", input_file_name, await fetchFile(file!.item(0)!));
+        ffmpeg.FS("writeFile", input_file_name, await fetchFile(file));
 
         await ffmpeg.run("-i", input_file_name, "-vf", `scale=${width}:${height}`, output_file_name);
         
@@ -27,7 +34,7 @@
         const linkEl = document.createElement("a");
         linkEl.href = URL.createObjectURL(
             new Blob([data.buffer], { 
-                type: "image/png" 
+                type: file.type 
             })
         );
         linkEl.download = output_file_name;
@@ -35,7 +42,7 @@
     }
 </script>
 
-<input type="file" bind:files={file} />
+<input type="file" bind:files={files} />
 
 <input type="number" bind:value={width} />
 <input type="number" bind:value={height} />
