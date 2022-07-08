857bd4a7706bcfe2e6b9ba4b8a0147bd91a3b2a5
Xyphuz <hansen0715.2@gmail.com>
2022-07-09 00:00:23 +0800

    chore: add css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,122 +1,171 @@
 <script lang="ts">
-    import "../app.css";
-    import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+	import '../app.css';
+	import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
-    let file: File | null | undefined = null;
+	let file: File | null | undefined = null;
 
-    let width = -1;
-    let height = -1;
+	let width = -1;
+	let height = -1;
 
-    async function convert() {
-        if (!validateFile()) {
-            return;
-        }
+	$: canConvert = false;
 
-        const inputFileName = file!.name;
-        const outputFileName: string = inputFileName.replace(/\.([^.]+)$/, '-resized.$1');
+	async function convert() {
+		if (!validateFile()) {
+			return;
+		}
 
-        const ffmpeg = createFFmpeg();
-        await ffmpeg.load();
+		const inputFileName = file!.name;
+		const outputFileName: string = inputFileName.replace(/\.([^.]+)$/, '-resized.$1');
 
-        ffmpeg.FS("writeFile", inputFileName, await fetchFile(file!));
+		const ffmpeg = createFFmpeg();
+		await ffmpeg.load();
 
-        await ffmpeg.run("-i", inputFileName, "-vf", `scale=${width}:${height}`, outputFileName);
-        
-        const data = await ffmpeg.FS("readFile", outputFileName);
+		ffmpeg.FS('writeFile', inputFileName, await fetchFile(file!));
 
-        const linkEl = document.createElement("a");
-        linkEl.href = URL.createObjectURL(
-            new Blob([data.buffer], { 
-                type: file!.type 
-            })
-        );
-        linkEl.download = outputFileName;
-        linkEl.click();
-    }
+		await ffmpeg.run('-i', inputFileName, '-vf', `scale=${width}:${height}`, outputFileName);
 
-    let dropZone: HTMLElement;
-    let dropZoneContent: HTMLElement;
-    let previewSection: HTMLImageElement;
+		const data = await ffmpeg.FS('readFile', outputFileName);
 
-    function onDragEnter() {
-        dropZone.classList.add("bg-gray-100");
-    }
+		const linkEl = document.createElement('a');
+		linkEl.href = URL.createObjectURL(
+			new Blob([data.buffer], {
+				type: file!.type
+			})
+		);
+		linkEl.download = outputFileName;
+		linkEl.click();
+	}
 
-    function onDragLeave(event: DragEvent) {
-        if (dropZone.contains(event.relatedTarget as Node)) {
-            return;
-        }
+	let dropZone: HTMLElement;
+	let dropZoneContent: HTMLElement;
+	let previewSection: HTMLImageElement;
 
-        dropZone.classList.remove("bg-gray-100");
-    }
+	function onDragEnter() {
+		dropZone.classList.add('bg-gray-100');
+	}
 
-    function onDrop(event: DragEvent) {
-        dropZone.classList.remove("bg-gray-100");
+	function onDragLeave(event: DragEvent) {
+		if (dropZone.contains(event.relatedTarget as Node)) {
+			return;
+		}
 
-        file = event.dataTransfer?.files?.item(0);
+		dropZone.classList.remove('bg-gray-100');
+	}
 
-        showPreview();
-    }
+	function onDrop(event: DragEvent) {
+		dropZone.classList.remove('bg-gray-100');
 
-    function onFileInputChange(event: Event) {
-        file = (event.target as HTMLInputElement).files?.item(0);
+		file = event.dataTransfer?.files?.item(0);
 
-        showPreview();
-    }
+		canConvert = validateFile();
+		if (!canConvert) {
+			return;
+		}
 
-    function validateFile(): boolean {
-        if (file === null || file === undefined) {
-            return false;
-        }
+		showPreview();
+	}
 
-        if (!file.type.startsWith('image/')) {
-            return false;
-        }
+	function onFileInputChange(event: Event) {
+		file = (event.target as HTMLInputElement).files?.item(0);
 
-        return true;
-    }
+		canConvert = validateFile();
+		if (!canConvert) {
+			return;
+		}
 
-    function showPreview() {
-        if (!validateFile()) {
-            return;
-        }
+		showPreview();
+	}
 
-        previewSection.classList.remove("hidden");
-        dropZoneContent.classList.add("hidden");
+	function validateFile(): boolean {
+		if (file === null || file === undefined) {
+			return false;
+		}
 
-        previewSection.src = URL.createObjectURL(file!);
-    }
+		if (!file.type.startsWith('image/')) {
+			return false;
+		}
+
+		return true;
+	}
+
+	function showPreview() {
+		previewSection.classList.remove('hidden');
+		dropZoneContent.classList.add('hidden');
+
+		previewSection.src = URL.createObjectURL(file!);
+	}
 </script>
 
 <svelte:head>
-    <title>Image Resizer</title>
+	<title>Image Resizer</title>
 </svelte:head>
 
-<div class="flex items-center justify-center h-screen w-screen">
-    <div class="flex flex-col items-center justify-center w-96">
-        <div class="flex justify-center items-center w-full"
-        on:dragenter={onDragEnter}
-        on:dragleave={onDragLeave}
-        on:dragover|preventDefault
-        on:drop|preventDefault={onDrop}
-        >
-            <label for="file-input" class="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600" bind:this={dropZone}
-            >
-                <div class="flex flex-col justify-center items-center pt-5 pb-6" bind:this={dropZoneContent}>
-                    <svg class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                    <p class="text-xs text-gray-500 dark:text-gray-400">Support any type of image.</p>
-                </div>
-                <input id="file-input" type="file" class="hidden" on:change={onFileInputChange} />
-                <img class="hidden w-full h-full object-contain"  src="" alt="" bind:this={previewSection} />
-            </label>
-        </div> 
+<div
+	class="flex items-center justify-center w-screen h-screen bg-gradient-to-r from-orange-600 via-red-500 to-pink-400"
+>
+	<div
+		class="flex flex-col items-center justify-center w-[calc(min(48rem,100%))] h-128 bg-white rounded-2xl shadow-2xl p-6"
+	>
+		<div
+			class="flex justify-center items-center w-full h-full"
+			on:dragenter={onDragEnter}
+			on:dragleave={onDragLeave}
+			on:dragover|preventDefault
+			on:drop|preventDefault={onDrop}
+		>
+			<label
+				for="file-input"
+				class="flex flex-col justify-center items-center w-full h-full bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+				bind:this={dropZone}
+			>
+				<div
+					class="flex flex-col justify-center items-center pt-5 pb-6"
+					bind:this={dropZoneContent}
+				>
+					<svg
+						class="mb-3 w-10 h-10 text-gray-400"
+						fill="none"
+						stroke="currentColor"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+						/></svg
+					>
+					<p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+						<span class="font-semibold">Click to upload</span> or drag and drop
+					</p>
+					<p class="text-xs text-gray-500 dark:text-gray-400">Support any type of image.</p>
+				</div>
+				<input id="file-input" type="file" class="hidden" on:change={onFileInputChange} />
+				<img class="hidden h-96 object-scale-down" src="" alt="" bind:this={previewSection} />
+			</label>
+		</div>
 
-        <input type="number" bind:value={width} />
-        <input type="number" bind:value={height} />
+		<div class="flex flex-row flex-wrap mt-6 w-full">
+			<input
+				class="flex-1 shadow-md rounded px-2 py-2 border-2 border-gray-300 bg-gray-50 mr-4"
+				type="number"
+				bind:value={width}
+			/>
+			<input
+				class="flex-1 shadow-md rounded px-2 py-2 border-2 border-gray-300 bg-gray-50 mr-4"
+				type="number"
+				bind:value={height}
+			/>
 
-        <button on:click="{convert}">
-            CONVERT
-        </button>
-    </div>
+			<button
+				class="flex-1 shadow-md bg-gradient-to-l from-orange-600 via-red-500 to-pink-400 opacity-90 text-white font-bold py-2 px-4 rounded"
+				class:opacity-40={!canConvert}
+				class:hover:opacity-100={canConvert}
+				disabled={!canConvert}
+				on:click={convert}
+			>
+				CONVERT
+			</button>
+		</div>
+	</div>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '128': '32rem',
+      }
+    }
   },
   plugins: [],
 }
