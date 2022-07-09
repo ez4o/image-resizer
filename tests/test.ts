142d7d6ca943308a60f7b744e6dfc5f6e71da872
Xyphuz <hansen0715.2@gmail.com>
2022-07-09 15:49:09 +0800

    chore: add simple test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,23 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+test('test', async ({ page }) => {
+	// Go to http://localhost:3000/
+	await page.goto('http://localhost:3000/');
+
+	// Click label:has-text("Click to upload or drag and drop Support any type of image.")
+	await page
+		.locator('label:has-text("Click to upload or drag and drop Support any type of image.")')
+		.click();
+
+	// Click input[type="number"] >> nth=0
+	await page.locator('input[type="number"]').first().click();
+
+	// Fill input[type="number"] >> nth=0
+	await page.locator('input[type="number"]').nth(1).fill('100');
+
+	// Click div:has-text("Click to upload or drag and drop Support any type of image. CONVERT") >> nth=1
+	await page
+		.locator('div:has-text("Click to upload or drag and drop Support any type of image. CONVERT")')
+		.nth(1)
+		.click();
 });
